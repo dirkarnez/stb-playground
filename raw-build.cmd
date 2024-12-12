452ec40a06b2d0be8fe513484628ca35562fd480
dirkarnez <smalldirkalex@gmail.com>
2024-12-12 15:19:50 +0800

    - add .github/workflows/build.yml

--- raw-build.cmd
+++ raw-build.cmd
@@ -3,9 +3,9 @@
 @REM set PREFIX=D:\Softwares
 set PREFIX=%USERPROFILE%\Downloads
 
-if not exist build mkdir build
+if not exist raw-build mkdir raw-build
 
-%PREFIX%\winlibs-x86_64-posix-seh-gcc-11.2.0-mingw-w64-9.0.0-r1\mingw64\bin\gcc.exe -I%~dp0third-parties\stb_image_write\v1.16 -c src\main.c -o build\main.o &&^
-%PREFIX%\winlibs-x86_64-posix-seh-gcc-11.2.0-mingw-w64-9.0.0-r1\mingw64\bin\gcc.exe build\main.o -o build\helloworld.exe -static &&^
+%PREFIX%\winlibs-x86_64-posix-seh-gcc-11.2.0-mingw-w64-9.0.0-r1\mingw64\bin\gcc.exe -I%~dp0third-parties\stb_image_write\v1.16 -c src\main.c -o raw-build\main.o &&^
+%PREFIX%\winlibs-x86_64-posix-seh-gcc-11.2.0-mingw-w64-9.0.0-r1\mingw64\bin\gcc.exe raw-build\main.o -o raw-build\helloworld.exe -static &&^
 pause
 
