27e1a4e6427607d1092ed5442bca47e8f716e85e
dirkarnez <smalldirkalex@gmail.com>
2024-12-12 15:15:03 +0800

    - change to gcc

--- raw-build.cmd
+++ raw-build.cmd
@@ -5,7 +5,7 @@ set PREFIX=%USERPROFILE%\Downloads
 
 if not exist build mkdir build
 
-%PREFIX%\winlibs-x86_64-posix-seh-gcc-11.2.0-mingw-w64-9.0.0-r1\mingw64\bin\g++.exe -I%~dp0third-parties\stb_image_write\v1.16 -c src\main.c -o build\main.o &&^
-%PREFIX%\winlibs-x86_64-posix-seh-gcc-11.2.0-mingw-w64-9.0.0-r1\mingw64\bin\g++.exe build\main.o -o build\helloworld.exe -static &&^
+%PREFIX%\winlibs-x86_64-posix-seh-gcc-11.2.0-mingw-w64-9.0.0-r1\mingw64\bin\gcc.exe -I%~dp0third-parties\stb_image_write\v1.16 -c src\main.c -o build\main.o &&^
+%PREFIX%\winlibs-x86_64-posix-seh-gcc-11.2.0-mingw-w64-9.0.0-r1\mingw64\bin\gcc.exe build\main.o -o build\helloworld.exe -static &&^
 pause
 
